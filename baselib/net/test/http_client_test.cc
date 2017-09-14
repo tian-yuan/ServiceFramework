c09b2d46dbe760229a2c4334962208a4c1e2a4d9
liuzebo <hzliuzebo@corp.netease.com>
2017-09-14 10:00:20 +0800

    add httpclient test code

---
+++ baselib/net/test/http_client_test.cc
@@ -0,0 +1,19 @@
+#include "net/io_thread_pool_manager.h"
+#include "net/server/http_client.h"
+#include <iostream>
+
+void http_callback(HttpClientReplyData& replyData) {
+    std::cout << "receive result !" << std::endl;
+    std::cout << replyData.ToString() << std::endl;
+}
+
+int main() {
+    std::shared_ptr<DefaultIOThreadPoolExecutor> threadPool = std::make_shared<DefaultIOThreadPoolExecutor>();
+    folly::EventBase* evb = threadPool->GetEventBase(1);
+    auto main_evb = folly::EventBaseManager::get()->getEventBase();
+    main_evb->runAfterDelay([&]{
+        HttpClientGet(main_evb, "http://www.baidu.com", http_callback);
+    }, 1000);
+    main_evb->loopForever();
+    return 0;
+}
